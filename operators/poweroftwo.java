8d351acf46638c4ad8d2bcc390707252558ba434
sharavana18 <sharavanakumar2020@gmail.com>
2025-12-13 09:42:58 +0530

    Delete operators/poweroftwo.java

--- operators/poweroftwo.java
+++
@@ -1,8 +0,0 @@
-import java.util.Scanner;
-public class poweroftwo {
-    public static void main (String[] args){
-    Scanner sc = new Scanner(System.in);
-    int n = sc.nextInt();
-    System.err.println(((n&(n-1))==0)?"power of two":"Not power of two");
-}
-}
