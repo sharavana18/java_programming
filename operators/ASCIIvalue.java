c1681bc16eac1ab350b14ed2e2da225c92bf461b
sharavana18 <sharavanakumar2020@gmail.com>
2025-12-13 09:37:45 +0530

    Delete operators/ASCIIvalue.java

--- operators/ASCIIvalue.java
+++
@@ -1,9 +0,0 @@
-import java.util.Scanner;
-public class ASCIIvalue {
-        public static void main(String[] args){
-        Scanner sc = new Scanner(System.in);
-        char Str = sc.next().charAt(0);
-        System.out.print((int)Str);
-        sc.close();
-}
-} 
