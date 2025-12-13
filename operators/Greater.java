b221bb3da8b9440b9c21086cb1a76a5a4333468d
sharavana18 <sharavanakumar2020@gmail.com>
2025-12-13 10:18:50 +0530

    Add files via upload

---
+++ operators/Greater.java
@@ -0,0 +1,13 @@
+
+        import java.util.Scanner;
+public class Greater {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nexttInt();
+        int c = sc.nextInt();
+        System.out.println((a>b)?(a<c)?a:b:c);
+        
+    }
+}
+
