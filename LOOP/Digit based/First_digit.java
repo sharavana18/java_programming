a9a01502d100c9b9a9ef52ac9017cc92926672b2
sharavana18 <sharavanakumar2020@gmail.com>
2025-12-17 12:03:09 +0530

    Delete LOOP directory

--- LOOP/Digit based/First_digit.java
+++
@@ -1,9 +0,0 @@
-import java.util.*;
-class First_digit{
- public static void main(String[] args){
-  Scanner sc=new Scanner(System.in);
-  int n=sc.nextInt();
-  while(n>=10) n/=10;
-  System.out.println(n);
- }
-}
