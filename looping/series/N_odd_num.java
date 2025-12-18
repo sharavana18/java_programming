9e3156a9a7d6ebfe570f9687de2fde47871d890e
sharavana18 <sharavanakumar2020@gmail.com>
2025-12-18 09:23:35 +0530

    Add files via upload

---
+++ looping/series/N_odd_num.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class N_odd_num{
+public static void main(String[] args){
+    Scanner sc = new Scanner(System.in);
+    int n = sc.nextInt();
+    int s;
+    for(int i=1;i<=n;i++){
+        s = (i*2)-1;
+        System.out.print(s+" ");
+    }
+    }
+}
---
+++ looping/series/Reverse_Natural_num.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class Reverse_Natural_num{
+public static void main(String[] args){
+    Scanner sc = new Scanner(System.in);
+    int n = sc.nextInt();
+    for(int i=n;i<=n;i--)
+    if(i>0)
+    System.out.print(i+" ");
+    }
+}
